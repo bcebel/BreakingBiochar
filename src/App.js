a52de3c07a7ebe954bf779822c9159d047aff57e
bcebel <bcebel@gmail.com>
2024-05-25 23:05:55 -0500

    earth background, backdrops css etc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import earth from "./earth.jpg";
 
 function App() {
   return (
@@ -14,41 +15,45 @@ function App() {
             in a low-oxygen environment, a process known as pyrolysis. It has
             numerous environmental benefits, including...
           </p>
+          <h2>Approach</h2>
+          <p>
+            Carbon Capture and Storage by producing Biochar, a carbon-rich
+            material produced by heating organic matter in a low-oxygen
+            environment, a process known as pyrolysis. Combining Biochar with
+            soil and compost can improve soil health, increase crop yields, and
+            sequester carbon dioxide from the atmosphere.
+          </p>
 
+          <h2>Benefits</h2>
           <p>
-            <h2>Approach</h2>Carbon Capture and Storage by producing Biochar, a
-            carbon-rich material produced by heating organic matter in a
-            low-oxygen environment, a process known as pyrolysis. Combining
-            Biochar with soil and compost can improve soil health, increase crop
-            yields, and sequester carbon dioxide from the atmosphere.
+            Global Warming, Soil Health, Energy, Landfill Diversion, Water
+            Quality, Waste Management, Sustainable Agriculture, Climate Change
+            Mitigation, Carbon Sequestration, Carbon Footprint Reduction, Carbon
+            Negative, Carbon Neutral, Carbon Credits, Carbon Offsetting, Carbon
+            Farming, Carbon Markets, Carbon Pricing, Carbon Tax, Carbon Trading,
+            Carbon Emissions, Greenhouse Gas Reduction, Renewable Energy, Clean
+            Energy, Low Carbon Economy, Net Zero Emissions, Negative Emissions
+            Due to the formula of C02, for every 1 ton of Biochar produced, 3
+            tons of C02 are sequestered (Oxygen is heavier than Carbon and there
+            are 2 Oxygen for every 1 Carbon in CO2)
           </p>
+
+          <h2>Costs</h2>
           <p>
-            <h2>Benefits</h2>Global Warming, Soil Health, Energy, Landfill
-            Diversion, Water Quality, Waste Management, Sustainable Agriculture,
-            Climate Change Mitigation, Carbon Sequestration, Carbon Footprint
-            Reduction, Carbon Negative, Carbon Neutral, Carbon Credits, Carbon
-            Offsetting, Carbon Farming, Carbon Markets, Carbon Pricing, Carbon
-            Tax, Carbon Trading, Carbon Emissions, Greenhouse Gas Reduction,
-            Renewable Energy, Clean Energy, Low Carbon Economy, Net Zero
-            Emissions, Negative Emissions Due to the formula of C02, for every 1
-            ton of Biochar produced, 3 tons of C02 are sequestered (Oxygen is
-            heavier than Carbon and there are 2 Oxygen for every 1 Carbon in
-            CO2)
-            <p>
-              <h2>Costs</h2>Production, Transportation, Storage, Distribution,
-              Marketing, Research, Development, Testing, Certification,
-              Compliance, Regulation, Policy, Legislation, Incentives,
-              Subsidies, Grants, Loans, Investments, Partnerships,
-              Collaborations, Alliances, Joint Ventures, Mergers, Acquisitions,
-              Divestitures,
-            </p>
-            <p>
-              <h2>Competition</h2>The government lol, Direct Air Carbon Capture
-              & Storage, Bioenergy with Carbon Capture and Storage BECCS, CCS,
-              Soil Amendments, Landfill Gas Capture, Phytoremediation, Water
-              Treatment
-            </p>
+            Production, Transportation, Storage, Distribution, Marketing,
+            Research, Development, Testing, Certification, Compliance,
+            Regulation, Policy, Legislation, Incentives, Subsidies, Grants,
+            Loans, Investments, Partnerships, Collaborations, Alliances, Joint
+            Ventures, Mergers, Acquisitions, Divestitures,
           </p>
+
+          <h2>Competition</h2>
+          <p>
+            The government lol, Direct Air Carbon Capture & Storage, Bioenergy
+            with Carbon Capture and Storage BECCS, CCS, Soil Amendments,
+            Landfill Gas Capture, Phytoremediation, Water Treatment
+          </p>
+
           <p>
             Biochar can be used for green energy production, carbon
             sequestration, and improving soil health.
